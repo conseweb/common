9266e51b2b990bf63d1554059651005497df9cb0
xiebo <xiebo@qkldx.net>
2016-11-09 18:44:21 +0800

    修改：完善功能，支持多种加密算法，可配置;

--- assets/poe/README.md
+++ assets/poe/README.md
@@ -1,1 +1,127 @@
-# 存在性证明
+## Proof of Existence Service（存在性证明服务）
+
+### developer test
+
+run fabric peer using docker
+
+docker-commpose.yml
+```
+membersrvc:
+  image: hyperledger/fabric-membersrvc
+  ports:
+    - "7054:7054"
+  command: membersrvc
+vp0:
+  image: hyperledger/fabric-peer
+  ports:
+    - "7050:7050"
+    - "7051:7051"
+    - "7053:7053"
+  environment:
+    - CORE_PEER_ADDRESSAUTODETECT=true
+    - CORE_VM_ENDPOINT=unix:///var/run/docker.sock
+    - CORE_LOGGING_LEVEL=DEBUG
+    - CORE_PEER_ID=vp0
+    - CORE_PEER_PKI_ECA_PADDR=membersrvc:7054
+    - CORE_PEER_PKI_TCA_PADDR=membersrvc:7054
+    - CORE_PEER_PKI_TLSCA_PADDR=membersrvc:7054
+    - CORE_SECURITY_ENABLED=false
+	- CORE_SECURITY_ENROLLID=test_vp0
+    - CORE_SECURITY_ENROLLSECRET=MwYpmSRjupbT
+  links:
+    - membersrvc
+  volumes_from:
+    - share_data
+  command: sh -c "sleep 5; peer node start --peer-chaincodedev"
+```
+start docker
+```
+docker-compose up
+```
+build poe and run locally
+```
+CORE_CHAINCODE_ID_NAME=poe_cc_demo_001 CORE_PEER_ADDRESS=0.0.0.0:7051 ./poe
+```
+```
+docker ps -a
+docker exec -it xxxx bash
+```
+### deploy request
+
+REST
+```
+{
+    "jsonrpc": "2.0",
+    "method": "deploy",
+    "params": {
+        "type": 1,
+        "chaincodeID": {
+            "name": "poe_cc_demo_001"
+        },
+        "ctorMsg": {
+            "function": "deploy",
+            "args": [
+            ]
+        },
+        "secureContext": ""
+    },
+    "id": 1
+}
+```
+
+### invoke request
+
+#### invoke_register
+REST
+```
+{
+    "jsonrpc": "2.0",
+    "method": "invoke",
+    "params": {
+        "type": 1,
+        "chaincodeID": {
+            "name": "poe_cc_demo_001"
+        },
+        "ctorMsg": {
+            "function": "register",
+            "args": ["xiebo","wangchuanjian","zhaoming"]
+        },
+        "secureContext": ""
+    },
+    "id": 1
+}
+```
+
+### query request
+#### query_existence
+
+REST
+```
+{
+    "jsonrpc": "2.0",
+    "method": "query",
+    "params": {
+        "type": 1,
+        "chaincodeID": {
+            "name": "poe_cc_demo_001"
+        },
+        "ctorMsg": {
+            "function": "existence",
+            "args": ["xiebo","wangchuanjian","zhaoming"]
+        },
+        "secureContext": ""
+    },
+    "id": 1
+}
+```
+Response
+```
+{
+    "jsonrpc": "2.0",
+    "result": {
+        "status": "OK",
+        "message": "[{\"key\":\"xiebo\",\"exist\":true},{\"key\":\"wangchuanjian\",\"exist\":true},{\"key\":\"zhaoming\",\"exist\":true}]"
+    },
+    "id": 1
+}
+```
--- assets/poe/main.go
+++ assets/poe/main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"crypto/md5"
-	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"hash"
 	"strings"
 
+	"github.com/conseweb/common/config"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/flogging"
 	"github.com/op/go-logging"
+	"github.com/spf13/viper"
+	"golang.org/x/crypto/ripemd160"
+	"golang.org/x/crypto/sha3"
 )
 
-var logger = logging.MustGetLogger("PoeService")
+// 日志记录器
+var logger = logging.MustGetLogger("poe")
 
-// Proof of Existence Service
+// 哈稀函数标记
+const (
+	HASH_FUNC_FLAGS_RIPEMD160 = "RIPEMD160"
+	HASH_FUNC_FLAGS_SHA3_224  = "SHA3_224"
+	HASH_FUNC_FLAGS_SHA3_256  = "SHA3_256"
+	HASH_FUNC_FLAGS_SHA3_384  = "SHA3_384"
+	HASH_FUNC_FLAGS_SHA3_512  = "SHA3_512"
+	HASH_FUNC_FLAGS_MD5       = "MD5"
+)
+
+// Proof of Existence Service（存在性证明服务）
 type PoeService struct{}
 
 func (this *PoeService) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
@@ -49,30 +65,22 @@ const (
 
 //注册键值
 func (this *PoeService) register(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var (
-		param string = args[0]
-		array []string
-	)
 	// 验证非空
-	if len(strings.TrimSpace(param)) == 0 {
+	if len(args) == 0 {
 		return nil, errors.New("func <register> Parameter is not valid,Cannot be empty or contain null characters")
 	}
-	array = strings.Split(param, ",")
+	//logger.Warningf("<register> parameters :%s", args)
 	// 循环存入
-	for _, key := range array {
-		m := StoreEntity{}
-		m.Key = hashMd5(key)
-		m.Length = int64(len(key))
-		m.Value = []byte(key)
-		data, e := json.Marshal(&m)
-		if e != nil {
-			logger.Errorf("func <register> error:%s", e.Error())
-			break
-		}
-		e = stub.PutState(m.Key, data)
-		if e != nil {
-			logger.Errorf("func <register> error:%s", e.Error())
-			break
+	for _, key := range args {
+		if len(strings.TrimSpace(key)) > 0 {
+			hkey, e := hashKey(key)
+			if e != nil {
+				return nil, e
+			}
+			e = stub.PutState(hkey, []byte{1})
+			if e != nil {
+				return nil, e
+			}
 		}
 	}
 	return nil, nil
@@ -80,43 +88,83 @@ func (this *PoeService) register(stub shim.ChaincodeStubInterface, args []string
 
 //检索键值是否存在
 func (this *PoeService) existence(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var (
-		param string = args[0]
-		array []string
-		list  []QueryEntity
-	)
+	var list []QueryResult
 	// 验证非空
-	if len(strings.TrimSpace(param)) == 0 {
+	if len(args) == 0 {
 		return nil, errors.New("func <existence> Parameter is not valid,Cannot be empty or contain null characters")
 	}
-	array = strings.Split(param, ",")
-	for _, key := range array {
-		data, e := stub.GetState(hashMd5(key))
+	//logger.Warningf("<existence> parameters :%s", args)
+	for _, key := range args {
+		hkey, e := hashKey(key)
 		if e != nil {
 			return nil, errors.New("func <existence> error:" + e.Error())
 		}
-		if data != nil && len(data) > 0 {
-			m := QueryEntity{}
+		d, e := stub.GetState(hkey)
+		if e != nil {
+			return nil, errors.New("func <existence> error:" + e.Error())
+		}
+		if d != nil && len(d) > 0 {
+			m := QueryResult{}
 			m.Key = key
-			m.IsExist = true
+			m.Exist = true
 			list = append(list, m)
 		}
 	}
-	return nil, nil
+	data, e := json.Marshal(&list)
+	if e != nil {
+		return nil, errors.New("func <existence> error:" + e.Error())
+	}
+	return data, nil
 }
 
-// MD5 Hash
-func hashMd5(data string) string {
-	hash := md5.New()
-	hash.Write([]byte(data))
-	cpiherText := hash.Sum(nil)
-	hexText := make([]byte, 32)
-	hex.Encode(hexText, cpiherText)
-	return string(hexText)
+// 获取传入key 的哈希值
+// hash1+hash2+len(key)
+func hashKey(key string) (string, error) {
+	mainHashConf := viper.GetString("hash_func.main_hash")
+	mainHash := hashDevice(mainHashConf)
+	_, e := mainHash.Write([]byte(key))
+	if e != nil {
+		return "", e
+	}
+	subHashConf := viper.GetString("hash_func.sub_hash")
+	subHash := hashDevice(subHashConf)
+	mainStr := string(mainHash.Sum(nil))
+	_, e = subHash.Write([]byte(key))
+	if e != nil {
+		return "", e
+	}
+	subStr := string(subHash.Sum(nil))
+	return mainStr + subStr + string(len(key)), nil
+}
+
+// 根据哈希函数标记构造具体的哈希实例
+func hashDevice(flags string) hash.Hash {
+	switch flags {
+	case HASH_FUNC_FLAGS_RIPEMD160:
+		return ripemd160.New()
+	case HASH_FUNC_FLAGS_SHA3_224:
+		return sha3.New224()
+	case HASH_FUNC_FLAGS_SHA3_256:
+		return sha3.New256()
+	case HASH_FUNC_FLAGS_SHA3_384:
+		return sha3.New384()
+	case HASH_FUNC_FLAGS_SHA3_512:
+		return sha3.New512()
+	case HASH_FUNC_FLAGS_MD5:
+		return md5.New()
+	default:
+		return md5.New()
+	}
 }
 
 func main() {
-	if err := shim.Start(new(PoeService)); err != nil {
-		logger.Errorf("start Lepuscoin return error: %v, exiting\n", err)
+	e := config.LoadConfig("", "poe", "github.com/conseweb/common/assets/poe")
+	if e != nil {
+		logger.Errorf("start PoeService return error: %v, exiting\n", e)
+		return
+	}
+	flogging.LoggingInit("poe")
+	if e = shim.Start(new(PoeService)); e != nil {
+		logger.Errorf("start PoeService return error: %v, exiting\n", e)
 	}
 }
